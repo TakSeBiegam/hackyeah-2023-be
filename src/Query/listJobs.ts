ddf461620a21e998478ff138eab5b01d81835bf8
TakSeBiegam <kurylo.ao@gmail.com>
2023-10-01 11:32:17 +0200

    :ambulance: hotfix

--- src/Query/listJobs.ts
+++ src/Query/listJobs.ts
@@ -5,12 +5,5 @@ import { orm } from '../utils/db/orm.js';
 export const handler = async (input: FieldResolveInput) =>
   resolverFor('Query', 'listJobs', async (args) => {
     const jobs = await orm().then((o) => o('Jobs').collection.find({}).sort({ createdAt: 'desc' }).toArray());
-    return {
-      _id: 'NOT',
-      files: jobs[0].files,
-      createdAt: jobs[0].createdAt,
-      tuneId: jobs[0].tuneId,
-      conversationCountWhenCreated: jobs[0].conversationCountWhenCreated,
-      fineTuneModel: jobs[0].fineTuneModel,
-    };
+    return jobs[0].fineTuneModel;
   })(input.arguments);
